9ac2e69ef026edcea7e7d34254ca224cf7b2fa22
MatusRepkaSolutions <162296215+MatusRepkaSolutions@users.noreply.github.com>
2024-03-12 11:25:48 +0100

    Update connect.php

--- ESP basics/4. SQL+ESP/POST request/PHP/connect.php
+++ ESP basics/4. SQL+ESP/POST request/PHP/connect.php
@@ -6,7 +6,7 @@ $dbPassword = "your_db_login_password";
 $dbName     = "your_db_name";  
   
 /* Create a $db variable you can then use in any code on your website once you include it.
-If you are unsure how this works, visit our github - SQL basics - for easy tutorials*/
+If you are unsure how this works, visit our repository - 0. PHP basics - for easy tutorials*/
 $db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);  
   
 // If there was an error in the connection, this will let you know
